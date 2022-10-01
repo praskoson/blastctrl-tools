ed6abb7f65ef78361bdc70fe09d30dde7fe3921d
praskoson <40672437+praskoson@users.noreply.github.com>
2022-10-01 11:00:19 +0200

    fix: transparent header

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -29,7 +29,7 @@ export const HomeView: FC = ({}) => {
   return (
     <div className='md:hero mx-auto p-4'>
       <div className='md:hero-content flex flex-col'>
-        <h1 className='text-center text-5xl font-bold font-display text-transparent text-slate-700'>
+        <h1 className='text-center text-5xl font-bold font-display text-slate-700'>
           BlastCtrl <span>Tools</span>
         </h1>
         <h4 className='md:w-full text-center text-slate-600 my-2'>
