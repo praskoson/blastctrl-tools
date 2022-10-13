9869cf37853be81293b50ea1ba82d43283bb112b
praskoson <40672437+praskoson@users.noreply.github.com>
2022-10-14 01:01:29 +0200

    refactoring

--- src/hooks/useUserNfts.ts
+++ src/hooks/useUserNfts.ts
@@ -1,40 +1,40 @@
-import { Connection, PublicKey } from '@solana/web3.js';
-import { Metaplex, Cluster, guestIdentity } from '@metaplex-foundation/js';
-import { useConnection, useWallet } from '@solana/wallet-adapter-react';
-import useSWR from 'swr';
-import { useNetworkConfiguration } from 'contexts/NetworkConfigurationProvider';
+import { Connection, PublicKey } from "@solana/web3.js";
+import { Metaplex, Cluster, guestIdentity } from "@metaplex-foundation/js";
+import { useConnection, useWallet } from "@solana/wallet-adapter-react";
+import useSWR from "swr";
+import { useNetworkConfiguration } from "contexts/NetworkConfigurationProvider";
+import { useMemo } from "react";
 
-const fetchWalletNfts =
-  (connection: Connection, cluster: Cluster) => async (wallet58: string) => {
-    let wallet: PublicKey;
-    try {
-      wallet = new PublicKey(wallet58);
-    } catch (err) {
-      throw Error(`Invalid wallet pubkey: ${wallet58}`, {
-        cause: err as Error,
-      });
-    }
+const fetchWalletNfts = (connection: Connection, cluster: Cluster) => async (wallet58: string) => {
+  let wallet: PublicKey;
+  try {
+    wallet = new PublicKey(wallet58);
+  } catch (err) {
+    throw Error(`Invalid wallet pubkey: ${wallet58}`, {
+      cause: err as Error,
+    });
+  }
 
-    const metaplex = Metaplex.make(connection, { cluster }).use(
-      guestIdentity()
-    );
-    
-    // Can be NFT | SFT | "token with metadata"
-    return await metaplex
-      .nfts()
-      .findAllByOwner({ owner: wallet, commitment: 'confirmed' })
-      .run();
-  };
+  const metaplex = Metaplex.make(connection, { cluster }).use(guestIdentity());
+
+  // Can be NFT | SFT | "token with metadata"
+  return await metaplex.nfts().findAllByOwner({ owner: wallet, commitment: "confirmed" }).run();
+};
 
 export const useUserNfts = () => {
   const { connection } = useConnection();
   const { publicKey } = useWallet();
   const { networkConfiguration } = useNetworkConfiguration();
 
-  const fetcher = fetchWalletNfts(connection, networkConfiguration);
+  const fetcher = useMemo(
+    () => fetchWalletNfts(connection, networkConfiguration),
+    [connection, networkConfiguration]
+  );
 
   const { data, error } = useSWR(() => publicKey?.toBase58(), fetcher, {
     refreshInterval: 60000,
+    errorRetryCount: 5,
+    errorRetryInterval: 10000,
   });
 
   return { isError: error, isLoading: !error && !data, nfts: data };
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,11 +3,11 @@ import Head from "next/head";
 import { FC } from "react";
 import { ContextProvider } from "../contexts/ContextProvider";
 import { AppBar } from "../components/AppBar";
-import { ContentContainer } from "../components/ContentContainer";
 import { Footer } from "../components/Footer";
 import Notifications from "../components/Notification";
 import relativeTime from "dayjs/plugin/relativeTime";
 import dayjs from "dayjs";
+import Link from "next/link";
 // import { Breadcrumbs } from 'components/Breadcrumbs';
 
 dayjs.extend(relativeTime);
@@ -22,15 +22,43 @@ const App: FC<AppProps> = ({ Component, pageProps }) => {
       </Head>
 
       <ContextProvider>
-        <div className="flex flex-col h-screen">
+        <div className="flex h-screen flex-col">
           <Notifications />
           <AppBar />
           {/* <Breadcrumbs /> */}
-          <ContentContainer>
-            <Component {...pageProps} />
-            <Footer />
-          </ContentContainer>
+          <div className="drawer">
+            <input id="my-drawer" type="checkbox" className="drawer-toggle" />
+            <div className="drawer-content">
+              <Component {...pageProps} />
+            </div>
+
+            {/* SideBar / Drawer */}
+            <div className="drawer-side">
+              <label htmlFor="my-drawer" className="drawer-overlay"></label>
+              <ul className="menu w-80 overflow-y-auto bg-base-100 p-4">
+                <li>
+                  <h1>Menu</h1>
+                </li>
+                <li>
+                  <Link href="/">
+                    <a>Home</a>
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/collections">
+                    <a>Collections</a>
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/arweave">
+                    <a>Arweave</a>
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
         </div>
+        {/* <Footer></Footer> */}
       </ContextProvider>
     </>
   );
