799e7f3d052dea119e209f13500ef632e1ff47c3
praskoson <40672437+praskoson@users.noreply.github.com>
2022-10-23 17:03:15 +0200

    fix

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -63,12 +63,12 @@ export const HomeView: FC = ({}) => {
               <div className="flex max-w-md flex-col flex-wrap gap-2 sm:flex-row">
                 <Link href="/nft-tools/update">
                   <a className="inline-flex items-center justify-center rounded-lg border-2 border-indigo-600 px-3 py-1.5 text-indigo-900 shadow hover:bg-indigo-50 sm:justify-start">
-                    Update Metadata
+                    Update NFT Metadata
                   </a>
                 </Link>
                 <Link href="/nft-tools/collections">
                   <a className="inline-flex items-center justify-center rounded-lg border-2 border-cyan-600 px-3 py-1.5 text-cyan-900 shadow hover:bg-cyan-50">
-                    Add NFTs to on-chain collections
+                    Add/Remove NFT from collection
                   </a>
                 </Link>
                 <Link href="/storage/file-upload">
