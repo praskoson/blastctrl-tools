9a94803f023f2daf82569f84c1fb3d4145d6016e
praskoson <prasinar@gmail.com>
2022-10-26 01:04:03 +0200

    autocomplete?

--- src/components/NftSelector.tsx
+++ src/components/NftSelector.tsx
@@ -59,7 +59,7 @@ export const NftSelector = (
             type="text"
             data-lpignore="true"
             placeholder="Token"
-            autoComplete="false"
+            autoComplete="off"
             className={classNames(
               "w-full rounded-md border border-gray-300 bg-white py-2 pl-3 pr-10 shadow-sm",
               "focus:border-indigo-500 focus:outline-none focus:ring-1 focus:ring-indigo-500 sm:text-sm",
