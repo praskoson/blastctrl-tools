d4890e68b0211d8067064b0a0d618d00f43e34e9
praskoson <40672437+praskoson@users.noreply.github.com>
2022-11-26 20:13:50 +0100

    Bump wallet adapter packages

--- src/contexts/ContextProvider.tsx
+++ src/contexts/ContextProvider.tsx
@@ -1,41 +1,51 @@
-import {
-  WalletAdapterNetwork,
-  WalletError,
-  WalletSendTransactionError,
-} from "@solana/wallet-adapter-base";
+import { WalletError, WalletSendTransactionError } from "@solana/wallet-adapter-base";
 import { ConnectionProvider, WalletProvider } from "@solana/wallet-adapter-react";
 import { WalletModalProvider as ReactUIWalletModalProvider } from "@solana/wallet-adapter-react-ui";
-import {
-  PhantomWalletAdapter,
-  SolflareWalletAdapter,
-  SolletWalletAdapter,
-  GlowWalletAdapter,
-} from "@solana/wallet-adapter-wallets";
-import { LedgerWalletAdapter } from "@solana/wallet-adapter-ledger";
 import { FC, ReactNode, useCallback, useMemo } from "react";
 import { AutoConnectProvider, useAutoConnect } from "./AutoConnectProvider";
 import { mergeClusterApiUrl } from "utils/spl/common";
 import { useNetworkConfigurationStore } from "stores/useNetworkConfiguration";
 import { notify } from "components/Notification";
+import {
+  BackpackWalletAdapter,
+  GlowWalletAdapter,
+  PhantomWalletAdapter,
+  SolflareWalletAdapter,
+  LedgerWalletAdapter,
+  BraveWalletAdapter,
+  SalmonWalletAdapter,
+} from "@solana/wallet-adapter-wallets";
 
 const WalletContextProvider: FC<{ children: ReactNode }> = ({ children }) => {
   const { autoConnect } = useAutoConnect();
   const { network } = useNetworkConfigurationStore();
-  // const { networkConfiguration } = useNetworkConfiguration();
-  // endpo
-  // const endpoint = useMemo(() => mergeClusterApiUrl(networkConfiguration), [networkConfiguration]);
   const endpoint = useMemo(() => mergeClusterApiUrl(network), [network]);
 
   const wallets = useMemo(
     () => [
+      /**
+       * Wallets that implement either of these standards will be available automatically.
+       *
+       *   - Solana Mobile Stack Mobile Wallet Adapter Protocol
+       *     (https://github.com/solana-mobile/mobile-wallet-adapter)
+       *   - Solana Wallet Standard
+       *     (https://github.com/solana-labs/wallet-standard)
+       *
+       * If you wish to support a wallet that supports neither of those standards,
+       * instantiate its legacy wallet adapter here. Common legacy adapters can be found
+       * in the npm package `@solana/wallet-adapter-wallets`.
+       */
       new PhantomWalletAdapter(),
       new SolflareWalletAdapter(),
-      new SolletWalletAdapter(),
       new GlowWalletAdapter(),
+      new BackpackWalletAdapter(),
       new LedgerWalletAdapter(),
-      // new SlopeWalletAdapter(),
+      new BackpackWalletAdapter(),
+      new BraveWalletAdapter(),
+      new SalmonWalletAdapter(),
     ],
-    []
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [network]
   );
 
   const onError = useCallback((error: WalletError) => {
