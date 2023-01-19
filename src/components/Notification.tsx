571f82fcbfe267d56d9889218332f94dd8b0b729
praskoson <prasinar@gmail.com>
2023-01-19 01:37:20 +0100

    promise notification api

--- src/components/Notification.tsx
+++ src/components/Notification.tsx
@@ -6,18 +6,9 @@ import {
 } from "@heroicons/react/24/outline";
 import { XMarkIcon } from "@heroicons/react/20/solid";
 import useQueryContext from "hooks/useQueryContext";
-import toast from "react-hot-toast";
+import toast, { resolveValue, ValueOrFunction } from "react-hot-toast";
 import { classNames } from "utils";
 import { SpinnerIcon } from "./SpinnerIcon";
-
-export const notify = (props: NotifyProps) => {
-  return toast.custom(
-    (t) => (
-      <NotificationWindow {...props} visible={t.visible} onClose={() => toast.dismiss(t.id)} />
-    ),
-    { duration: props.type === "error" ? Infinity : 15000 }
-  );
-};
 
 export type NotifyProps = {
   type?: "error" | "success" | "loading" | "info";
@@ -43,7 +34,7 @@ export function NotificationWindow({
       case "error":
         return <ExclamationTriangleIcon className="h-7 w-7 text-amber-600" aria-hidden="true" />;
       case "loading":
-        return <SpinnerIcon className="h-7 w-7 text-white" aria-hidden="true" />;
+        return <SpinnerIcon className="h-6 w-6 animate-spin text-white" aria-hidden="true" />;
       default:
         return <InformationCircleIcon className="h-7 w-7 text-cyan-400" aria-hidden="true" />;
     }
@@ -75,7 +66,7 @@ export function NotificationWindow({
           <div className="mt-1 flex-shrink-0">{iconMemo}</div>
           <div className="ml-3 w-0 flex-1 pt-0.5">
             <p className="text-xl font-semibold tracking-wide text-gray-50">{title}</p>
-            <p className="mt-1 text-sm text-gray-300">{description}</p>
+            <div className="mt-1 text-sm text-gray-300">{description}</div>
             {txid && <span className="block pt-4 pb-3 text-gray-200">{linkMemo}</span>}
           </div>
           <div className="ml-4 flex flex-shrink-0">
@@ -93,3 +84,35 @@ export function NotificationWindow({
     </div>
   );
 }
+
+export const notify = (props: NotifyProps, id?: string) => {
+  return toast.custom(
+    (t) => (
+      <NotificationWindow {...props} visible={t.visible} onClose={() => toast.dismiss(t.id)} />
+    ),
+    { duration: props.type === "loading" ? Infinity : 20000, id }
+  );
+};
+
+export const notifyPromise = <T,>(
+  promise: Promise<T>,
+  msgs: {
+    loading: NotifyProps;
+    success: ValueOrFunction<NotifyProps, T>;
+    error: ValueOrFunction<NotifyProps, any>;
+  }
+) => {
+  const id = notify({ type: "loading", ...msgs.loading });
+
+  promise
+    .then((value) => {
+      const result = resolveValue(msgs.success, value);
+      notify({ type: "success", ...result }, id);
+    })
+    .catch((err) => {
+      const result = resolveValue(msgs.error, err);
+      notify({ type: "error", ...result }, id);
+    });
+
+  return promise;
+};
