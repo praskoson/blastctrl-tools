5917e827f82a24c397e68703c06cfc1e08da335e
praskoson <prasinar@gmail.com>
2023-01-21 18:23:10 +0100

    utility hook: use data fetch

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -2,3 +2,5 @@ export * from "./useQueryContext";
 export * from "./useUserNfts";
 export * from "./useNftJson";
 export * from "./useUserNftsQn";
+export * from "./useDataFetch";
+export * from "./useWindowFocus";
