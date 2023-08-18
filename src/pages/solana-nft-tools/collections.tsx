28c43c088627a9c905ffe0ae11d1c38fe3347bb1
praskoson <prasinar@gmail.com>
2023-08-18 14:36:03 +0200

    wip

--- src/pages/solana-nft-tools/collections.tsx
+++ src/pages/solana-nft-tools/collections.tsx
@@ -1,15 +1,41 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import { CollectionsView } from "../../views";
+import { useState } from "react";
+import { Tab} from "@headlessui/react"
+import { AddTo } from "views/collections/AddTo";
+import { RemoveFrom } from "views/collections/RemoveFrom";
 
 const Collections: NextPage = () => {
+  const [tab, setTab] = useState<"add" | "remove">("add");
+
   return (
     <div>
       <Head>
         <title>Tools | Blast Ctrl - Collections</title>
         <meta name="Metaplex Collections" content="Basic Functionality" />
       </Head>
-      <CollectionsView />
+      <div className="mx-auto p-4 md:hero">
+        <div className="flex flex-col md:hero-content">
+          <div className="tabs">
+            <a
+              className={`tab tab-bordered ${tab === "add" ? "tab-active" : ""}`}
+              onClick={() => setTab("add")}
+            >
+              Add
+            </a>
+            <a
+              className={`tab tab-bordered ${tab === "remove" ? "tab-active" : ""}`}
+              onClick={() => setTab("remove")}
+            >
+              Remove
+            </a>
+          </div>
+          <div className="text-left">
+            {tab === "add" && <AddTo />}
+            {tab === "remove" && <RemoveFrom />}
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
